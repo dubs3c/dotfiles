7bf7b2180578eedabdc2944aa35829dddeca9331
dubsec <dubsec@localhost>
2020-01-07 00:03:09 +0100

    let there be light

---
+++ vim/.vimrc
@@ -0,0 +1,18 @@
+syntax on
+
+set background=dark
+
+set tabstop=4 " number of visual spaces per TAB
+set softtabstop=4   " number of spaces in tab when editing
+set expandtab       " tabs are spaces
+
+set number              " show line numbers
+
+"set cursorline          " highlight current line
+
+filetype indent on      " load filetype-specific indent files
+
+set lazyredraw          " redraw only when we need to.
+
+set incsearch           " search as characters are entered
+set hlsearch            " highlight matches
---
+++ zsh/.zsh/01-environment.zsh
@@ -0,0 +1,43 @@
+#                 ██      
+#                ░██      
+#  ██████  ██████░██      
+# ░░░░██  ██░░░░ ░██████  
+#    ██  ░░█████ ░██░░░██ 
+#   ██    ░░░░░██░██  ░██ 
+#  ██████ ██████ ░██  ░██ 
+# ░░░░░░ ░░░░░░  ░░   ░░  
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ author ▓ xero <x@xero.nu>
+# ░▓ code   ▓ http://code.xero.nu/dotfiles
+# ░▓ mirror ▓ http://git.io/.files
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+#
+#█▓▒░ timestamps
+#HIST_STAMPS=yyyy/mm/dd
+
+#█▓▒░ paths
+export PATH=/usr/local/bin:$HOME/bin:$HOME/go/bin/:$HOME/.local/bin:$PATH
+#export MANPATH=/usr/local/man:$MANPATH
+
+#█▓▒░ preferred text editor
+export EDITOR=nvim
+export VISUAL=nvim
+
+#█▓▒░ language
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=sv_SE.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LESSCHARSET=utf-8
+
+#█▓▒░ go lang
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+
